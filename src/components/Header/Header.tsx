0908b494e774778c6ff7cff6265cc38aabbc4be1
Jose Luis <joseluis.barreche@gmail.com>
2019-08-15 18:53:45 +0200

    Update all npm dependencies. Update linting for sorted react props

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ class Header extends React.Component {
     render() {
         return (
             <div>
-                <img src={butterflyIcon} alt="" />
+                <img alt="" src={butterflyIcon} />
                 <span className={'icon-home'} />
                 <h1 className={'header'}>Wow World! This is TypeScript boilerplate (Header component). Using TS!</h1>
             </div>
