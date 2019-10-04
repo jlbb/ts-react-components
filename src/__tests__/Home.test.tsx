b9fd59319845a9e24d40e422fa94864b44f30152
Jose Luis <joseluis.barreche@gmail.com>
2019-10-04 14:47:46 +0200

    Add a basic test for Home component. Update necessary dependencies and jest configuration

---
+++ src/__tests__/Home.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { render, wait } from '@testing-library/react';
+import { MockedProvider } from '@apollo/react-testing';
+import Home from '../components/Home/Home';
+
+describe('Home', () => {
+    test('element renders', () => {
+        const { container } = render(
+            <MockedProvider>
+                <Home />
+            </MockedProvider>,
+        );
+        wait();
+        expect(container.firstChild).toBeDefined();
+    });
+});
