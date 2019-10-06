8f140e7a13c7d78c97e41a8549e69e71da9f8730
Jose Luis <joseluis.barreche@gmail.com>
2019-10-06 23:57:54 +0200

    Add more tests for hooks

--- src/__tests__/hooks.test.tsx
+++ src/__tests__/hooks.test.tsx
@@ -2,24 +2,55 @@
 // https://github.com/apollographql/react-apollo/issues/2867
 // TODO: Check invariant violation happened when adding ToDo object, and addToDo performs the cache update
 import { act, renderHook } from '@testing-library/react-hooks';
-import { useAddToDoMutation } from '../graphql/ToDo/hooks';
-import { ToDo, ToDos } from '../types/types';
+import {
+    useAddToDoItemMutation,
+    useAddToDoMutation,
+    useRemoveToDoItemMutation,
+    useRemoveToDoMutation,
+    useUpdateToDoItemMutation,
+} from '../graphql/ToDo/hooks';
+import { cleanup } from '@testing-library/react';
+import { ToDo, ToDoItemInput, ToDos } from '../types/types';
 import { MockedProvider, MockedResponse } from '@apollo/react-testing';
-import { ADD_TODO, ROOT_GET_TODO_LIST_QUERY } from '../graphql/ToDo/queries';
+import {
+    ADD_TODO,
+    ADD_TODO_ITEM,
+    REMOVE_TODO,
+    REMOVE_TODO_ITEM,
+    ROOT_GET_TODO_LIST_QUERY,
+    UPDATE_TODO_ITEM,
+} from '../graphql/ToDo/queries';
 
 const newToDo: ToDo = {
     id: '2',
     name: 'New ToDo hooks test list',
     toDoList: [],
 };
+const newToDoItemInput: ToDoItemInput = {
+    description: 'Hook new ToDoItem',
+};
+const newToDoItem = {
+    id: '1',
+    ...newToDoItemInput,
+    completed: false,
+    __typename: 'ToDoItem',
+};
+
+const toDoItem = {
+    id: 'a1',
+    description: 'ToDo item to remove',
+    completed: true,
+    __typename: 'ToDoItem',
+};
+const toDoItemUpdated = { ...toDoItem, description: 'Updated ToDo item' };
+const toDo: ToDo = {
+    id: 'a1',
+    name: 'Test ToDo list',
+    toDoList: [toDoItem],
+};
+
 const data: ToDos = {
-    toDos: [
-        {
-            id: 'a1',
-            name: 'Test ToDo list',
-            toDoList: [],
-        },
-    ],
+    toDos: [toDo],
 };
 
 const mocks: MockedResponse[] = [
@@ -42,7 +73,55 @@ const mocks: MockedResponse[] = [
             data: { addToDo: newToDo },
         },
     },
+    {
+        request: {
+            query: REMOVE_TODO,
+            variables: {
+                id: toDo.id,
+            },
+        },
+        result: {
+            data: { removeToDo: toDo },
+        },
+    },
+    {
+        request: {
+            query: ADD_TODO_ITEM,
+            variables: {
+                idToDo: toDo.id,
+                toDoItem: newToDoItemInput,
+            },
+        },
+        result: {
+            data: { addToDoItem: newToDoItem },
+        },
+    },
+    {
+        request: {
+            query: REMOVE_TODO_ITEM,
+            variables: {
+                idToDo: toDo.id,
+                idToDoItem: toDoItem.id,
+            },
+        },
+        result: {
+            data: { removeToDoItem: toDoItem },
+        },
+    },
+    {
+        request: {
+            query: UPDATE_TODO_ITEM,
+            variables: {
+                idToDo: toDo.id,
+                toDoItem: toDoItemUpdated,
+            },
+        },
+        result: {
+            data: { updateToDoItem: toDoItemUpdated },
+        },
+    },
 ];
+
 const wrapHook = () => {
     const component = MockedProvider;
     component.defaultProps = {
@@ -52,8 +131,10 @@ const wrapHook = () => {
     return component;
 };
 
+afterEach(cleanup);
+
 describe('hooks', () => {
-    test('useAddToMutation', async () => {
+    test('useAddToDoMutation', async () => {
         const {
             result: { current: addToDo },
         } = renderHook(useAddToDoMutation, { wrapper: wrapHook() });
@@ -63,8 +144,63 @@ describe('hooks', () => {
                 data: { addToDo: resToDo },
             }: any = await addToDo(newToDo.name);
 
-            // console.log('res', resToDo);
             expect(resToDo).toEqual(newToDo);
+        });
+    });
+
+    test('removeAddToDoMutation', async () => {
+        const {
+            result: { current: removeToDo },
+        } = renderHook(useRemoveToDoMutation, { wrapper: wrapHook() });
+
+        await act(async () => {
+            const {
+                data: { removeToDo: resToDo },
+            }: any = await removeToDo(toDo.id);
+
+            expect(resToDo).toEqual(toDo);
+        });
+    });
+
+    test('useAddToDoItemMutation', async () => {
+        const {
+            result: { current: addToDoItem },
+        } = renderHook(useAddToDoItemMutation, { wrapper: wrapHook() });
+
+        await act(async () => {
+            const {
+                data: { addToDoItem: resToDo },
+            }: any = await addToDoItem(toDo.id, newToDoItemInput);
+
+            expect(resToDo).toEqual(newToDoItem);
+        });
+    });
+
+    test('useRemoveToDoItemMutation', async () => {
+        const {
+            result: { current: removeToDoItem },
+        } = renderHook(useRemoveToDoItemMutation, { wrapper: wrapHook() });
+
+        await act(async () => {
+            const {
+                data: { removeToDoItem: resToDo },
+            }: any = await removeToDoItem(toDo.id, toDoItem.id);
+
+            expect(resToDo).toEqual(toDoItem);
+        });
+    });
+
+    test('useUpdateToDoItemMutation', async () => {
+        const {
+            result: { current: updateToDoItem },
+        } = renderHook(useUpdateToDoItemMutation, { wrapper: wrapHook() });
+
+        await act(async () => {
+            const {
+                data: { updateToDoItem: resToDo },
+            }: any = await updateToDoItem(toDo.id, toDoItemUpdated);
+
+            expect(resToDo).toEqual(toDoItemUpdated);
         });
     });
 });
